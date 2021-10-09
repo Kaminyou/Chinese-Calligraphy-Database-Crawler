16434a402a82ba5bc687ddcbbcfd710ace9587e6
Kaminyou <ikaminyou@gmail.com>
2021-10-09 08:56:57 +0800

    add crawler

---
+++ crawl.py
@@ -0,0 +1,14 @@
+import requests
+
+if __name__ == "__main__":
+    with open("mapping.csv", "w") as f:
+        for i in range(100000):
+            r = requests.get(f"https://www.cns11643.gov.tw/wordWrite.jsp?ID={i}")
+            if "<caption>法帖查詢：" in r.text:
+                idx = r.text.find("<caption>法帖查詢：")
+                extract = r.text[idx:idx+30]
+                word = extract.split("[")[1].split("]")[0]
+                print(f"{i:<6} || {word}")
+                f.write(f"{i},{word}\n")
+            else:
+                print(f"{i:<6} || None")
